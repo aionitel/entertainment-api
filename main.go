1e858b1b725c9b91de7fda745cf4aaa907aa99ba
Alex Ionitel <oranggsoda@gmail.com>
2022-04-08 19:58:05 -0400

    added gin and basic hello world server example

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
 func main() {
-	println("Hello, world.")
+	r := gin.New()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
 }
