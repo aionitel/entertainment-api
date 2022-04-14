7135febef81cc1bbc1b728be8334764f53d01987
Alex Ionitel <oranggsoda@gmail.com>
2022-04-14 12:13:37 -0400

    serving html/tmpl files working

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/oranges0da/go-server/routes"
+)
 
 func main() {
 	router := gin.New()
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", routes.Home)
+
+	router.Run(":4000")
 }
--- routes/home.go
+++ routes/home.go
@@ -1,1 +1,9 @@
 package routes
+
+import "github.com/gin-gonic/gin"
+
+func Home(c *gin.Context) {
+	c.HTML(200, "home.tmpl", gin.H{
+		"title": "Home Page",
+	})
+}
