7f5ad86e4a1bb46a7a2912981981d26468214e35
Alex Ionitel <oranggsoda@gmail.com>
2022-05-07 20:58:05 -0400

    added godotenv

--- service/movie/GetMovie.go
+++ service/movie/GetMovie.go
@@ -1,20 +1,21 @@
 package service
 
 import (
-	"encoding/json"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/oranges0da/entertainment-api/model"
 )
 
-// get movie by name in query param
-func GetMovieByTitle(client *http.Client, title string) error {
+// get movie details by name in query param
+func GetMovieByTitle(client *http.Client, title string) {
 	key := os.Getenv("API_KEY")
 
 	var url string = "http://www.omdbapi.com/?t=" + title + "&apikey=" + key
+
+	log.Print(url)
 
 	req, err := http.NewRequest("GET", url, nil) // create request, not send it
 
@@ -24,17 +25,11 @@ func GetMovieByTitle(client *http.Client, title string) error {
 
 	res, err := client.Do(req) // send request
 
-	if err != nil {
-		panic(err)
-	}
+	defer res.Body.Close()
 
-	defer res.Body.Close() // close body after request is done
+	body, err := ioutil.ReadAll(res.Body)
 
-	body, err := ioutil.ReadAll(res.Body) // read body
-
-	data := json.Unmarshal(body, &model.Movie{})
-
-	return data
+	log.Print(string(body))
 }
 
 // get movie by imdb id in query param
@@ -47,10 +42,10 @@ func GetMovie() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		movieTitle := c.Query("title")
 
-		data := GetMovieByTitle(http.DefaultClient, movieTitle)
+		GetMovieByTitle(http.DefaultClient, movieTitle)
 
 		c.JSON(200, gin.H{
-			"data": data,
+			"data": movieTitle,
 		})
 	}
 }
