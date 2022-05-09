b98010d502174534f5fdf69c011ad02fc13e266e
Alex Ionitel <oranggsoda@gmail.com>
2022-05-09 19:25:01 -0400

    finished book model

---
+++ service/book/Book.go
@@ -0,0 +1,49 @@
+package model
+
+type Book struct {
+	Title          string   `json:"title"`
+	Authors        []string `json:"authors"`
+	Publisher      string   `json:"publisher"`
+	PublishedDate  string   `json:"publishedDate"`
+	Desc           string   `json:"description"`
+	PageCount      int      `json:"pageCount"`
+	PrintType      string   `json:"printType"`
+	Categories     []string `json:"categories"`
+	AverageRating  float32  `json:"averageRating"`
+	MaturityRating string   `json:"maturityRating"`
+	Cover          string   `json:"imageLinks"`
+	Language       string   `json:"language"`
+	PublicDomain   bool     `json:"publicDomain"`
+}
+
+func NewBook(
+	title string,
+	authors []string,
+	publisher string,
+	publishedDate string,
+	desc string,
+	pageCount int,
+	printType string,
+	categories []string,
+	averageRating float32,
+	maturityRating string,
+	cover string,
+	language string,
+	publicDomain bool,
+) *Book {
+	return &Book{
+		Title:          title,
+		Authors:        authors,
+		Publisher:      publisher,
+		PublishedDate:  publishedDate,
+		Desc:           desc,
+		PageCount:      pageCount,
+		PrintType:      printType,
+		Categories:     categories,
+		AverageRating:  averageRating,
+		MaturityRating: maturityRating,
+		Cover:          cover,
+		Language:       language,
+		PublicDomain:   publicDomain,
+	}
+}
